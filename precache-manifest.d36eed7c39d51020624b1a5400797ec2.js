102da70ea015907cbb20591c2baee56e9c8b8b1e
heoheo.cheermsg <heoheo.cheermsg@gmail.com>
2019-06-10 03:45:31 +0900

    Updates

---
+++ precache-manifest.d36eed7c39d51020624b1a5400797ec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc5169b4c4474c29f546cd37f2bc2c51",
+    "url": "/fortunecookie/index.html"
+  },
+  {
+    "revision": "58860ea0d8e5b7603a1d",
+    "url": "/fortunecookie/static/css/main.12161e6a.chunk.css"
+  },
+  {
+    "revision": "7ee996b186ee25925c1f",
+    "url": "/fortunecookie/static/js/2.f0acff67.chunk.js"
+  },
+  {
+    "revision": "58860ea0d8e5b7603a1d",
+    "url": "/fortunecookie/static/js/main.dda49531.chunk.js"
+  },
+  {
+    "revision": "b9efcf13e371c8195064",
+    "url": "/fortunecookie/static/js/runtime~main.b3c2c40e.js"
+  },
+  {
+    "revision": "2d8a000d4f0b0e1839f46358d4970f24",
+    "url": "/fortunecookie/static/media/fortune.2d8a000d.jpg"
+  }
+]);
